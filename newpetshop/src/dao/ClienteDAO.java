eed65aec48c595aaa289e8484f5427f4deea156e
Hugo Ferreira Da Silva <81571386+hugoleao02@users.noreply.github.com>
2023-05-02 22:26:52 -0300

    Corrigindo compo  do insert do cliente

--- newpetshop/src/dao/ClienteDAO.java
+++ newpetshop/src/dao/ClienteDAO.java
@@ -10,54 +10,51 @@ import java.sql.PreparedStatement;
 import javax.swing.JOptionPane;
 import model.Cliente;
 
-
 public class ClienteDAO {
- 
-    public void inserirCliente(Cliente cliente)
-        
-{
-String sql= "INSERT INTO clientes(nome,cpf,raca,espece,dataNascimento,Enderecos_id)"
-             + " VALUES (?,?,?,?,?,?)";
-    
-   Connection conexao = null;
-   PreparedStatement pstmt = null;
-      
-    try {
-        conexao = FabricaConexao.criaConexao();
-        pstmt = conexao.prepareStatement(sql);
-         pstmt.setString(1,cliente.getNome());
-           pstmt.setString(2,cliente.getCpf());
-              pstmt.setString(3,cliente.getEmail());
-                 pstmt.setString(4,cliente.getTelefone());
-                    pstmt.setString(5,cliente.getDataNascimento());
-         pstmt.execute();
-         JOptionPane.showMessageDialog(null,
-                 "Dados Gravados com Sucesso!!!");
-    } catch (Exception e) {
-        e.printStackTrace(); 
-    }
-}
 
+    public void inserirCliente(Cliente cliente) {
+        String sql = "INSERT INTO clientes(nome,cpf,email,telefone,dataNascimento,Enderecos_id)"
+                + " VALUES (?,?,?,?,?,?)";
 
-public void alterarCliente(Cliente cliente)
-{             
- String sql= "UPDATE clientes SET nome=?,cpf=?,email=?,telefone=?,dataNascimento=?,Enderecos_id=? "
-         + "WHERE id=?";
-    
-    Connection conexao = null;
-    PreparedStatement pstmt = null;
-      
-    try {
-        conexao = FabricaConexao.criaConexao();
-        pstmt = conexao.prepareStatement(sql);
-        pstmt.setString(1,cliente.getNome());
-        pstmt.setString(2,cliente.getCpf());
-        pstmt.setString(3,cliente.getEmail());
-        pstmt.setString(4,cliente.getTelefone());
-        pstmt.setString(5,cliente.getDataNascimento());
-        pstmt.execute();
-    } catch (Exception e) {
-        e.printStackTrace(); 
+        Connection conexao = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conexao = FabricaConexao.criaConexao();
+            pstmt = conexao.prepareStatement(sql);
+            pstmt.setString(1, cliente.getNome());
+            pstmt.setString(2, cliente.getCpf());
+            pstmt.setString(3, cliente.getEmail());
+            pstmt.setString(4, cliente.getTelefone());
+            pstmt.setString(5, cliente.getDataNascimento());
+            
+            pstmt.execute();
+            
+            JOptionPane.showMessageDialog(null,
+                    "Dados Gravados com Sucesso!!!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
+
+    public void alterarCliente(Cliente cliente) {
+        String sql = "UPDATE clientes SET nome=?,cpf=?,email=?,telefone=?,dataNascimento=?,Enderecos_id=? "
+                + "WHERE id=?";
+
+        Connection conexao = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conexao = FabricaConexao.criaConexao();
+            pstmt = conexao.prepareStatement(sql);
+            pstmt.setString(1, cliente.getNome());
+            pstmt.setString(2, cliente.getCpf());
+            pstmt.setString(3, cliente.getEmail());
+            pstmt.setString(4, cliente.getTelefone());
+            pstmt.setString(5, cliente.getDataNascimento());
+            pstmt.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
